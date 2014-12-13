d583f619f71bc4f8f1f420d9bb6b3a47421eeb7f
albertcui <albertqcui+github@gmail.com>
2014-12-13 23:49:57 +0000

    add tweets to mongo

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var Twit = require('twit'),
     CartoDB = require('cartodb'),
     config = require('./config.js'),
-    app = require('express'),
-    nyc = [-74,40,-73,41]
+    express = require('express'),
+    path = require('path'),
+    db = require('monk')("mongodb://localhost/tweets"),
+    tweets = db.get('tweets'),
+    nyc = [-74,40,-73,41],
+    app = express(),
+    port = 8080
     //ny_place_id = "27485069891a7938"
 
 var T = new Twit({
@@ -12,13 +17,6 @@ var T = new Twit({
     access_token_secret:  config.twitter_token_secret
 })
 
-var C = new CartoDB({
-	user: 					config.cartodb_user,
-	api_key: 				config.cartodb_api_key
-})
-
-var stream = T.stream('statuses/filter', { locations: nyc })
-
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade');
 
@@ -27,45 +25,27 @@ app.route('/').get(function(req, res) {
   res.render('index')
 })
 
-C.on('connect', function() {
-    T.get('trends/place', { id: 2459115 }, function(err, data, response) {
-        data[0].trends.forEach(function(t){
-        	console.log(t);
-            T.get('search/tweets', {q: t.query, geocode: "40.772777,-73.952519,25mi", count:100}, function(err, data, response) {
-                data.statuses.forEach(function(tweet) {
-                	if (tweet.geo && tweet.geo.coordinates) {
-						C.query(
-							"INSERT INTO {table} (trend, display_name, tweet_id, text, latitude, longitude, user_avatar) \
-						    VALUES('{trend}', '{display_name}', {tweet_id}, '{text}', {latitude}, {longitude}, '{user_avatar}') ",
-						    {
-						    	table: 'tweets',
-						    	trend: t.name.replace("#", ""),
-						    	display_name: tweet.user.screen_name,
-						    	tweet_id: tweet.id_str,
-						    	text: tweet.text,
-						    	latitude: tweet.geo.coordinates[0],
-						    	longitude: tweet.geo.coordinates[1],
-						    	user_avatar: encodeURI(tweet.user.profile_image_url)
-						    },
-						    function(err, data){
-						        console.log(err)
-						        console.log(data)
-						    }
-						)
-					} else {
-						console.log("no coordinates")
-					}
+function getTweets() {
+            console.log("getting tweets")
+            T.get('trends/place', { id: 2459115 }, function(err, data, response) {
+            data[0].trends.forEach(function(t){
+                console.log(t);
+                T.get('search/tweets', {q: t.query, geocode: "40.772777,-73.952519,25mi", count:100}, function(err, data, response) {
+                    data.statuses.forEach(function(tweet) {
+                        if (tweet.geo && tweet.geo.coordinates) {
+                            tweets.insert(tweet)
+                        } else {
+                            console.log("no coordinates")
+                        }
+                    })
                 })
             })
         })
-    })
-})
+}
 
-C.connect()
+getTweets()
+setInterval(getTweets, 5 * 60 * 1000)
 
 app.listen(port, function() {
-    logger.info("[WEB] listening on port %s", port)
+    console.log("[WEB] listening on port 8080")
 })
-
-
-
