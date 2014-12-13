1e0ab541f40f4b0ef2b5f8d37ee2bf4be9f150ae
Albert Cui <aqc2109@columbia.edu>
2014-12-13 18:33:26 -0500

    add express, jade

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var Twit = require('twit'),
     CartoDB = require('cartodb'),
     config = require('./config.js'),
+    app = require('express'),
     nyc = [-74,40,-73,41]
     //ny_place_id = "27485069891a7938"
 
@@ -13,10 +14,18 @@ var T = new Twit({
 
 var C = new CartoDB({
 	user: 					config.cartodb_user,
-	api_key: 				config.cartodb_api_key 
+	api_key: 				config.cartodb_api_key
 })
 
 var stream = T.stream('statuses/filter', { locations: nyc })
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade');
+
+
+app.route('/').get(function(req, res) {
+  res.render('index')
+})
 
 C.on('connect', function() {
     T.get('trends/place', { id: 2459115 }, function(err, data, response) {
@@ -33,8 +42,8 @@ C.on('connect', function() {
 						    	trend: t.name.replace("#", ""),
 						    	display_name: tweet.user.screen_name,
 						    	tweet_id: tweet.id_str,
-						    	text: tweet.text, 
-						    	latitude: tweet.geo.coordinates[0], 
+						    	text: tweet.text,
+						    	latitude: tweet.geo.coordinates[0],
 						    	longitude: tweet.geo.coordinates[1],
 						    	user_avatar: encodeURI(tweet.user.profile_image_url)
 						    },
@@ -53,6 +62,10 @@ C.on('connect', function() {
 })
 
 C.connect()
+
+app.listen(port, function() {
+    logger.info("[WEB] listening on port %s", port)
+})
 
 
 
