6a22ab552f6a7ff15538304d0bec9ecdcb435f55
albertcui <albertqcui+github@gmail.com>
2014-12-13 22:06:01 +0000

    do trending query

--- app.js
+++ app.js
@@ -19,51 +19,59 @@ var C = new CartoDB({
 var stream = T.stream('statuses/filter', { locations: nyc })
 
 C.on('connect', function() {
-	stream.on('tweet', function (tweet) {
-		console.log(tweet);
-		if (tweet.geo && tweet.geo.coordinates) {
-			C.query(
-				"INSERT INTO {table} (display_name, tweet_id, text, latitude, longitude, user_avatar) \
-			    VALUES('{display_name}', {tweet_id}, '{text}', {latitude}, {longitude}, '{user_avatar}') ",
-			    {
-			    	table: 'tweets',
-			    	display_name: tweet.user.screen_name,
-			    	tweet_id: tweet.id_str,
-			    	text: tweet.text, 
-			    	latitude: tweet.geo.coordinates[0], 
-			    	longitude: tweet.geo.coordinates[1],
-			    	user_avatar: encodeURI(tweet.user.profile_image_url)
-			    },
-			    function(err, data){
-			        console.log(err)
-			        console.log(data)
+    T.get('trends/place', { id: 2459115 }, function(err, data, response) {
+        data.trends.forEach(function(t){
+            T.get('search/tweets', {q: t.query, geocode: [40.772777, -73.952519, 25]}, function(err, data, response){
+                console.log(data)
+            })
+        })
+    })
+// 	stream.on('tweet', function (tweet) {
+// 		console.log(tweet);
+// 		if (tweet.geo && tweet.geo.coordinates) {
+// 			C.query(
+// 				"INSERT INTO {table} (display_name, tweet_id, text, latitude, longitude, user_avatar) \
+// 			    VALUES('{display_name}', {tweet_id}, '{text}', {latitude}, {longitude}, '{user_avatar}') ",
+// 			    {
+// 			    	table: 'tweets',
+// 			    	display_name: tweet.user.screen_name,
+// 			    	tweet_id: tweet.id_str,
+// 			    	text: tweet.text, 
+// 			    	latitude: tweet.geo.coordinates[0], 
+// 			    	longitude: tweet.geo.coordinates[1],
+// 			    	user_avatar: encodeURI(tweet.user.profile_image_url)
+// 			    },
+// 			    function(err, data){
+// 			        console.log(err)
+// 			        console.log(data)
 
-			        if (tweet.entities.hashtags) {
-						tweet.entities.hashtags.forEach (function(hashtag) {
-							C.query(
-								"INSERT INTO {table}(tweet_id, hashtag) VALUES({tweet_id},'{hashtag}')",
-							    {
-							    	table: 'tweets_hashtag', 
-							    	tweet_id: tweet.id_str, 
-							    	hashtag: hashtag.text
-							    },
-							    function(err, data){
-							        console.log(err)
-							        console.log(data)
-							        process.exit()
-							    }
-					    	)
-					    })
-					}
-			    }
-	    	)
-		} else {
-			console.log("no coordinates")
-		}
-	})
+// 			        if (tweet.entities.hashtags) {
+// 						tweet.entities.hashtags.forEach (function(hashtag) {
+// 							C.query(
+// 								"INSERT INTO {table}(tweet_id, hashtag) VALUES({tweet_id},'{hashtag}')",
+// 							    {
+// 							    	table: 'tweets_hashtag', 
+// 							    	tweet_id: tweet.id_str, 
+// 							    	hashtag: hashtag.text
+// 							    },
+// 							    function(err, data){
+// 							        console.log(err)
+// 							        console.log(data)
+// 							        process.exit()
+// 							    }
+// 					    	)
+// 					    })
+// 					}
+// 			    }
+// 	    	)
+// 		} else {
+// 			console.log("no coordinates")
+// 		}
+// 	})
 
 })
 
 C.connect()
+
 
 
